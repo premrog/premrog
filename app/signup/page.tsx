dc94ebf2efb1cdce9aea4eed9701bdbb90892295
premrog <official.premrog@gmail.com>
2026-06-17 22:56:23 +0530

    Fix login page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import Link from "next/link";
 
 import { useRouter } from "next/navigation";
+
+import Link from "next/link";
 
 export default function SignupPage() {
     const router = useRouter();
